d103a0935e78301b4c778b68c470d2b7077c02ea
Sadao Tokuyama <tokuyama@1planet.co.jp>
2021-09-24 11:42:02 +0900

    commit

--- demo/js/modelviewer.js
+++ demo/js/modelviewer.js
@@ -5,7 +5,7 @@ class ARModelViewer {
        const ar_view = "ar-view";
        const not_access = "not-access";
        let display_id = "";
-       if (isAccess2AR())
+       if (ARCommon.isAccess2AR())
        {
            display_id = ar_view;
            ARCommon.loadAnalytics();
